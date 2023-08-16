4ebf3903c0a7cb8a5e9d8d23448c60ce5e928909
Arfan Lee <arfanlee@outlook.com>
2023-08-16 18:32:50 +0800

    Fixed wob check running, put X11 applications to run in the background

--- linux/scripts/autostart.sh
+++ linux/scripts/autostart.sh
@@ -6,7 +6,7 @@ pgrep -x emacs > /dev/null || emacs --daemon &
 # Check if power adapter is plugged in or not (this will run and done automatically, doesn't run in the background)
 $HOME/.local/bin/plug-check.sh
 # A script to notify when battery is low or full (use -f flag, or else it won't find it eventhough it's running)
-pgrep -f 'check-battery.sh' > /dev/null || $HOME/.local/bin/check_battery.sh &
+pgrep -f 'check_battery.sh' > /dev/null || $HOME/.local/bin/check_battery.sh &
 
 # Launch the application based on what server is on WAYLAND or Xorg
 # Running in Wayland
@@ -16,8 +16,7 @@ if [ -n "$WAYLAND_DISPLAY" ]; then
 
     # Wayland Overlay Bar setup
     WOBSOCK=$XDG_RUNTIME_DIR/wob.sock
-    # Use -x flag or it will not displaying after refrehsing wm
-    rm -f $WOBSOCK && mkfifo $WOBSOCK && pgrep -x $WOBSOCK > /dev/null || tail -f $WOBSOCK | wob &
+    pgrep -f $WOBSOCK > /dev/null || { rm -f $WOBSOCK; mkfifo $WOBSOCK; tail -f $WOBSOCK | wob & }
 
 	if [[ "$DESKTOP_SESSION" == "hyprland" ]]; then
 		pgrep -x waybar > /dev/null || waybar -c $HOME/.config/waybar/hyprconfig &
@@ -26,12 +25,13 @@ if [ -n "$WAYLAND_DISPLAY" ]; then
 	fi
 else
     # Running in Xorg
-    pgrep -x greenclip > /dev/null || greenclip --daemon
-    pgrep -x picom > /dev/null || picom -fc
-    pgrep -x dunst > /dev/null || dunst
+    pgrep -x greenclip > /dev/null || greenclip --daemon &
+    pgrep -x picom > /dev/null || picom -fc &
+    pgrep -x dunst > /dev/null || dunst &
+    $HOME/.fehbg
 
     if [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-		pgrep -x sxhkd > /dev/null || sxhkd
-		pgrep -x polybar > /dev/null || $HOME/.config/polybar/launch.sh
+		pgrep -x sxhkd > /dev/null || sxhkd &
+		pgrep -x polybar > /dev/null || $HOME/.config/polybar/launch.sh &
     fi
 fi
