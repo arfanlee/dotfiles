7533ad4296f47f9b8e8d507d96408d9dfa54f79f
Arfan Lee <arfanlee@outlook.com>
2023-08-16 18:33:53 +0800

    Put wob socket path into a variable, hence shortened it

--- linux/scripts/controller.sh
+++ linux/scripts/controller.sh
@@ -8,12 +8,14 @@ fi
 
 # Use the argument (input_value) directly in the script
 
+WOBSOCK=$XDG_RUNTIME_DIR/wob.sock
+
 # Lower Volume
 if [[ "$1" == "lv" ]]; then
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%- && wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100}' > $XDG_RUNTIME_DIR/wob.sock
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%- && wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100}' > $WOBSOCK
 # Raise Volume
 elif [[ "$1" == "rv" ]]; then
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+ && wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100}' > $XDG_RUNTIME_DIR/wob.sock
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+ && wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100}' > $WOBSOCK
 # Mute Speaker
 elif [[ "$1" == "ms" ]]; then
     wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
@@ -23,10 +25,10 @@ elif [[ "$1" == "mm" ]]; then
 
 # Brightness Up
 elif [[ "$1" == "bu" ]]; then
-    light -A 10 && light -G | cut -d'.' -f1 > $XDG_RUNTIME_DIR/wob.sock
+    light -A 10 && light -G | cut -d'.' -f1 > $WOBSOCK
 # Brightness Down
 elif [[ "$1" == "bd" ]]; then
-    light -U 10 && light -G | cut -d'.' -f1 > $XDG_RUNTIME_DIR/wob.sock
+    light -U 10 && light -G | cut -d'.' -f1 > $WOBSOCK
 else
     echo "Invalid input: '$1'"
 fi
