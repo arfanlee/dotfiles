d2ea545a0e1201e0d745ab0d1e80cb70fcac2341
Arfan Lee <arfanlee@outlook.com>
2023-08-23 22:59:00 +0800

    Added sound to check_battery.sh

--- linux/scripts/check_battery.sh
+++ linux/scripts/check_battery.sh
@@ -19,10 +19,12 @@ while true; do
 
     # show notification if battery low, file doesn't exists and it's discharging
     if [ $BATTERY_LEVEL -le 20 ] && [ $BATTERY_DISCHARGING -eq 1 ] && [ ! -f $LOW_FILE ]; then
+        setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-Warning.ogg >/dev/null 2>&1
         notify-send -i ~/.local/share/icons/Custom/low-battery.png -u critical "Battery Low" "Battery level is ${BATTERY_LEVEL}%. Please charge."
         touch $LOW_FILE
     # Send a notification on low battery
     elif [ $BATTERY_LEVEL -ge 95 ] && [ $BATTERY_DISCHARGING -eq 0 ] && [ ! -f $FULL_FILE ]; then
+        setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-App-Positive.ogg >/dev/null 2>&1
         notify-send -i ~/.local/share/icons/Custom/full-battery.png -u critical "Battery Full" "Battery level is ${BATTERY_LEVEL}%. You may unplug your charger."
         touch $FULL_FILE
     fi
