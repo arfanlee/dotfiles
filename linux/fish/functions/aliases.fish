791dfe1d14da99485ec97350dacf193f4bfc5392
Arfan Lee <arfanlee@outlook.com>
2023-08-24 20:54:14 +0800

    New fish aliases and abbreviations

--- linux/fish/functions/aliases.fish
+++ linux/fish/functions/aliases.fish
@@ -1,5 +1,5 @@
 # Aliases
-alias cp='cp -i'
+alias cp='cp -iv'
 alias la='ls -A'
 alias lla='ls -lA'
 alias ls='lsd'
@@ -8,12 +8,12 @@ alias mv='mv -i'
 alias rm='rm -i'
 
 # Abbreviations
+abbr co 'nvim ~/.config/'
 abbr dot 'cd ~/repos/dotfiles/linux'
 abbr lf 'lfcd'
 abbr sb 'feh --bg-fill'
 abbr sxiv 'sxiv -b'
+abbr untar 'tar xvf'
 abbr v 'nvim'
-abbr ins 'instaloader -l marryuiwanna --'
 abbr win 'sudo mount -t ntfs /dev/nvme0n1p3 /mnt'
 abbr wun 'sudo umount -t ntfs /dev/nvme0n1p3 /mnt'
-abbr untar 'tar xvf'
