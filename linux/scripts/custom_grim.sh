6959dea842f91771748145f82fd803325585894b
Arfan Lee <arfanlee@outlook.com>
2023-10-16 11:04:43 +0800

    Added argument for using grimshot with slurp

--- linux/scripts/custom_grim.sh
+++ linux/scripts/custom_grim.sh
@@ -6,8 +6,14 @@ filename=$(date +'Screenshot_%Y%m%d_%H%M%S.png')
 # Specify the directory to save the screenshot
 directory=$(xdg-user-dir)/Pictures/Screenshots/
 
-# Create the directory if it doesn't exist
-# mkdir -p "$directory"
-
+# If no argument, just screenshot whole screen
+if [ -z "$1" ]; then
+    grim "$directory$filename"
+    exit 1
+# Proper argument, would only screenshot within selected area
+elif [[ "$1" == "sl" ]]; then
+    grim -g "$(slurp)" "$directory$filename"
+else
+    echo "Invalid input: '$1'"
+fi
 # Take the screenshot using grim and save with the custom filename
-grim "$directory$filename"
