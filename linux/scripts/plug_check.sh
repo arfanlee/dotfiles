f242ef06970e12d75b84541c16841f8fc255d337
Arfan Lee <arfanlee@outlook.com>
2023-10-22 12:52:46 +0800

    Get the sounds to play!

--- linux/scripts/plug_check.sh
+++ linux/scripts/plug_check.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-export XAUTHORITY=/home/arfan/.Xauthority
-export DISPLAY=:0  # Ensure the correct display is set
+# Get all users active on the machine
+print_logged_users()
+{
+   who | grep "tty" | sed 's/^\s*\(\S\+\).*/\1/g' | uniq | sort
+}
 
-# Check if the laptop is unplugged
-if [[ $(cat /sys/class/power_supply/ADP0/online) == "0" ]]; then
-    # Laptop is unplugged
-    /usr/bin/aplay /usr/share/sounds/Oxygen-Sys-Warning.ogg &
-    /usr/bin/light -S 10  # Set brightness to 10%
-else
-    # Laptop is plugged in
-    /usr/bin/aplay /usr/share/sounds/Oxygen-Sys-App-Positive.ogg &
-    /usr/bin/light -S 100  # Set brightness to 100%
-fi
+declare -a logged_users=($(print_logged_users))
+
+for (( i=0; i<${#logged_users[@]}; i=($i + 1) )); do
+    cur_user=${logged_users[$i]}
+
+    # Check if the laptop is unplugged
+    if [[ $(cat /sys/class/power_supply/ADP0/online) == "0" ]]; then
+        su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-Warning.ogg >/dev/null 2>&1"
+        /usr/bin/light -S 10  # Set brightness to 10%
+    else
+        su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-App-Positive.ogg >/dev/null 2>&1"
+        /usr/bin/light -S 100  # Set brightness to 100%
+    fi
+done
