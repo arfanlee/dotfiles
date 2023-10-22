b71f4df75456dea93b9f3c8cc22ef7cc9a1dde32
Arfan Lee <arfanlee@outlook.com>
2023-10-22 20:49:17 +0800

    Fixed commands and switch around check for plug check and user check

--- linux/scripts/plug_check.sh
+++ linux/scripts/plug_check.sh
@@ -3,20 +3,29 @@
 # Get all users active on the machine
 print_logged_users()
 {
-   who | grep "tty" | sed 's/^\s*\(\S\+\).*/\1/g' | uniq | sort
+   # who | grep "tty" | sed 's/^\s*\(\S\+\).*/\1/g' | uniq | sort
+   # Another method
+   who | awk '{print $1}' | uniq | sort
 }
 
 declare -a logged_users=($(print_logged_users))
 
-for (( i=0; i<${#logged_users[@]}; i=($i + 1) )); do
-    cur_user=${logged_users[$i]}
-
-    # Check if the laptop is unplugged
-    if [[ $(cat /sys/class/power_supply/ADP0/online) == "0" ]]; then
-        su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-Warning.ogg >/dev/null 2>&1"
-        /usr/bin/light -S 10  # Set brightness to 10%
-    else
-        su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-App-Positive.ogg >/dev/null 2>&1"
-        /usr/bin/light -S 100  # Set brightness to 100%
+# Check if the laptop is unplugged
+if [[ $(cat /sys/class/power_supply/ADP0/online) == "0" ]]; then
+    if [[ "$1" == "play" ]]; then # Only play the sound when a value is given
+        for (( i=0; i<${#logged_users[@]}; i=($i + 1) )); do
+            cur_user=${logged_users[$i]}
+            su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-Warning.ogg >/dev/null 2>&1" &
+        done
     fi
-done
+    light -S 10 &               # Set brightness to 10%
+
+else
+    if [[ "$1" == "play" ]]; then
+        for (( i=0; i<${#logged_users[@]}; i=($i + 1) )); do
+            cur_user=${logged_users[$i]}
+            su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-App-Positive.ogg >/dev/null 2>&1" &
+        done
+    fi
+    light -S 100 &              # Set brightness to 100%
+fi
