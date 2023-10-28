2b0a65504503f0c949660093b7f3fed957b3e51d
Arfan Lee <arfanlee@outlook.com>
2023-10-28 23:21:05 +0800

    Fixed udev rules for plug

--- linux/scripts/plug_check.sh
+++ linux/scripts/plug_check.sh
@@ -13,22 +13,24 @@ print_logged_users()
 
 declare -a logged_users=($(print_logged_users))
 
-# Check if the laptop is unplugged
-if [[ $(cat /sys/class/power_supply/ADP0/online) == "0" ]]; then
-    if [[ "$1" == "play" ]]; then # Only play the sound when a value is given
-        for (( i=0; i<${#logged_users[@]}; i=($i + 1) )); do
-            cur_user=${logged_users[$i]}
-            su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-Warning.ogg >/dev/null 2>&1" &
-        done
-    fi
-    light -S 10 &               # Set brightness to 10%
+if [[ $(echo $XDG_SESSION_DESKTOP) != "" ]]; then
+    # Check if the laptop is unplugged
+    if [[ $(cat /sys/class/power_supply/ADP0/online) == "0" ]]; then
+        if [[ "$1" == "play" ]]; then # Only play the sound when a value is given
+            for (( i=0; i<${#logged_users[@]}; i=($i + 1) )); do
+                cur_user=${logged_users[$i]}
+                su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-Warning.ogg >/dev/null 2>&1" &
+            done
+        fi
+        light -S 10 &               # Set brightness to 10%
 
-else
-    if [[ "$1" == "play" ]]; then
-        for (( i=0; i<${#logged_users[@]}; i=($i + 1) )); do
-            cur_user=${logged_users[$i]}
-            su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-App-Positive.ogg >/dev/null 2>&1" &
-        done
+    else
+        if [[ "$1" == "play" ]]; then
+            for (( i=0; i<${#logged_users[@]}; i=($i + 1) )); do
+                cur_user=${logged_users[$i]}
+                su $cur_user -c "setsid -f ffplay -nodisp -autoexit /usr/share/sounds/Oxygen-Sys-App-Positive.ogg >/dev/null 2>&1" &
+            done
+        fi
+        light -S 100 &              # Set brightness to 100%
     fi
-    light -S 100 &              # Set brightness to 100%
 fi
