1c7482a32e6542c44a398597730de4f4dc4f9d43
Arfan Lee <arfanlee@outlook.com>
2023-11-09 22:07:35 +0800

    Added audio volume change sound

--- linux/scripts/controller.sh
+++ linux/scripts/controller.sh
@@ -5,9 +5,11 @@ WOBSOCK=$XDG_RUNTIME_DIR/wob.sock
 # Lower Volume
 if [[ "$1" == "lv" ]]; then     # Value from when using the script
     wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%- && wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100}' > $WOBSOCK
+    setsid -f ffplay -nodisp -autoexit /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga >/dev/null 2>&1 &
 # Raise Volume
 elif [[ "$1" == "rv" ]]; then
     wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+ && wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100}' > $WOBSOCK
+    setsid -f ffplay -nodisp -autoexit /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga >/dev/null 2>&1 &
 # Mute Speaker
 elif [[ "$1" == "ms" ]]; then
     wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
