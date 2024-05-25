8eba78fd7e6ce70b77a0932cad472e30de642ce4
Arfan Lee <arfanlee@outlook.com>
2024-05-25 17:04:34 +0800

    Updated config.fish - Renamed color function: set_ls_color => set_color - Changed bat: less => bat

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -1,16 +1,19 @@
+#!/bin/sh
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
 	# Sourcing
 	source ~/.config/fish/functions/keybindings.fish
 	source ~/.config/fish/functions/aliases.fish
-	source ~/.config/fish/functions/colors.fish # after source config.fish, need to run set_ls_colors (while in fish shell of course)
+	source ~/.config/fish/functions/colors.fish # after source config.fish, need to run set_color (while in fish shell of course)
 	source /usr/share/autojump/autojump.fish
 
+	export BROWSER="firefox"
+	export EDITOR="nvim"
 	export LANG=en_US.UTF-8
 	export LC_ALL=en_US.UTF-8
-	export EDITOR="nvim"
-	export BROWSER="firefox"
+    export PAGER="bat"
 
 	set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -f -g "" --depth 10'
 
