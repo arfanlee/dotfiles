b20ca5205af09c59582ae9e1187926dc46dd95b5
Arfan Lee <arfanlee@outlook.com>
2024-07-25 14:25:33 +0800

    Updated custom grim script - Added conditional for when slurp doesn't have an output; e.g: canceled selection—there will be no shutter sound

--- linux/scripts/custom_grim.sh
+++ linux/scripts/custom_grim.sh
@@ -3,19 +3,23 @@
 # Get the current date and time in the desired format
 filename=$(date +'Screenshot_%Y%m%d_%H%M%S.png')
 
-# Specify the directory to save the screenshot (Requires xdg-user-dirs command need to be downloaded)
+# Specify the directory to save the screenshot
 directory=$(xdg-user-dir)/Pictures/Screenshots/
-
 # If no argument, just screenshot whole screen
 if [ -z "$1" ]; then
 # Take a screenshot using grim and save with the custom path+filename
     grim "$directory$filename"
     setsid -f ffplay -nodisp -autoexit /usr/share/sounds/freedesktop/stereo/screen-capture.oga >/dev/null 2>&1 &
     exit 1
+
 # Proper argument, would only screenshot within selected area
 elif [[ "$1" == "sl" ]]; then
-    grim -g "$(slurp)" "$directory$filename"
-    setsid -f ffplay -nodisp -autoexit /usr/share/sounds/freedesktop/stereo/screen-capture.oga >/dev/null 2>&1 &
+    if [[ -n "$(slurp)" ]]; then  # Check if slurp output is not empty
+        grim -g "$(slurp)" "$directory$filename"
+        setsid -f ffplay -nodisp -autoexit /usr/share/sounds/freedesktop/stereo/screen-capture.oga >/dev/null 2>&1 &
+    else
+        exit  # Exit if slurp output is empty
+    fi
 else
     echo "Invalid input: '$1'"
 fi
