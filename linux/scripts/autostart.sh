321e25f4c7695ee98672bbe4893d24339aec5867
Arfan Lee <arfanlee@outlook.com>
2024-07-25 14:16:05 +0800

    Updated autostart script - Added temp file for on power or on battery checker - Removed clipman (no more updates) - Slight change with cliphist daemon - Added clipse daemon

--- linux/scripts/autostart.sh
+++ linux/scripts/autostart.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 # Launch applications on startup on any server when executed by window manager
+
 pgrep -x emacs > /dev/null || emacs --daemon &
 
 # Check if power adapter is plugged in or not, without sound
 $HOME/.local/bin/plug_check.sh &
+
+# To check on power or battery when in window manager
+WM_FILE="/tmp/6E6F746B6465"
+
+if [ ! -f $WM_FILE ]; then
+    touch $WM_FILE
+fi
 
 # A script to notify when battery is low or full (use -f flag, or else it won't find it eventhough it's running)
 pgrep -f 'check_battery.sh' > /dev/null || $HOME/.local/bin/check_battery.sh &
@@ -12,9 +20,9 @@ pgrep -f 'check_battery.sh' > /dev/null || $HOME/.local/bin/check_battery.sh &
 # Launch the application based on what server is on WAYLAND or Xorg
 # Running in Wayland
 if [ -n "$WAYLAND_DISPLAY" ]; then
-    # pgrep -x wl-paste > /dev/null || wl-paste -t text --watch clipman store -P --histpath="~/.local/share/clipman.json" &
-    pgrep -x wl-paste > /dev/null || wl-paste -t text --watch cliphist store &
-    pgrep -x wl-paste > /dev/null || wl-paste -t image --watch cliphist store &
+    pgrep -x wl-paste > /dev/null || wl-paste --type text --watch cliphist store &
+    pgrep -x wl-paste > /dev/null || wl-paste --type image --watch cliphist store &
+    # pgrep -x clipse > /dev/null || clipse -listen
     pgrep -x swaync > /dev/null || swaync &
 
     # Wayland Overlay Bar setup
@@ -22,7 +30,7 @@ if [ -n "$WAYLAND_DISPLAY" ]; then
     pgrep -f $WOBSOCK > /dev/null || { rm -f $WOBSOCK; mkfifo $WOBSOCK; tail -f $WOBSOCK | wob & }
 
 	if [[ "$DESKTOP_SESSION" == "hyprland" ]]; then
-        pgrep -x swaybg > /dev/null || swaybg -i $HOME/Pictures/Wallpapers/pastel-samurai.png -m fill &
+        pgrep -x swaybg > /dev/null || swaybg -i /path/to/image.jpg -m fill &
 		pgrep -x waybar > /dev/null || waybar -c $HOME/.config/waybar/hyprconfig &
 	else
 		pgrep -x waybar > /dev/null || waybar -c $HOME/.config/waybar/swayconfig &
